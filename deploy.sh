91b3b609c3ed5b7950ea7c6dc2f360c87181d181
David Salazar <davids1_4@hotmail.com>
2021-01-06 20:47:39 +0100

    fixed message

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo -e "Commit message: "
+echo "Commit message: "
 read message
 hugo --theme=hugo-future-imperfect-slim
 git add .
-git commit -m '$message'
+comillas = '"'
+git commit -m  "$message"
 git push origen master
