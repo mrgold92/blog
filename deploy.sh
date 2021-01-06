9472469156038bf1c38bb27fb355c3c67f1ed391
David Salazar <davids1_4@hotmail.com>
2021-01-06 18:38:06 +0100

    new post

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,4 @@
-#!/bin/sh
-
-# If a command fails then the deploy stops
-set -e
-
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-
-# Add changes to git.
+hugo --theme=hugo-future-imperfect-slim
 git add .
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin main
+git commit -m "new post"
+git push origen master
